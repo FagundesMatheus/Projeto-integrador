967192034e953e332dcf79f8b259f805476bd7bf
FagundesMatheus <fagundes2@discente.ufg.br>
2024-06-15 08:37:25 -0300

    botão corrigido

--- js/login.js
+++ js/login.js
@@ -1,45 +1,10 @@
-document.getElementById('form-login').addEventListener('submit', function (event) {
-    event.preventDefault(); // Impede o comportamento padrão de recarregar a página
-
-    const cpf = document.getElementById('cpf-input').value;
-    const password = document.getElementById('password-input').value;
-
-    if (cpf.length < 14) {
-        alert("Digite um cpf válido")
-        return
+// Certifique-se de que o DOM está completamente carregado
+document.addEventListener('DOMContentLoaded', function() {
+    var entrarBtn = document.getElementById('entrarBtn');
+    
+    if (entrarBtn) {
+        entrarBtn.addEventListener('click', function() {
+            window.location.href = 'home.html';
+        });
     }
-    if (password == "") {
-        alert("Digite uma senha")
-        return
-    }
-    window.location.href = 'home.html';
-})
-/*
-CONEXÃO COM O BACKEND - IGNORADO POR HORA
-
-fetch('/login', {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    body: new URLSearchParams({
-        'FieldCPF': cpf,
-        'FieldPassword': password
-    })
-})
-.then(response => {
-    if (response.ok) { // Verifica se a resposta do servidor é bem-sucedida
-        return response.text();
-    }
-    throw new Error('Network response was not ok.');
-})
-.then(data => {
-    // Verifica se há uma URL de redirecionamento na resposta
-    if (data.includes('redirected')) {
-        window.location.href = '/home';
-    } else {
-        console.log(data);
-    }
-})
-.catch(error => console.error('Error:', error));
-});*/
+});
